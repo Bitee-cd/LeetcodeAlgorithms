926ae08b95af40c64f0029ee004c154e1f4f5ba6
Bitee-cd <calebduniya45@gmail.com>
2022-02-14 23:54:26 +0100

    comments

--- 35 search index position.js
+++ 35 search index position.js
@@ -3,6 +3,13 @@
  * @param {number} target
  * @return {number}
  */
+// loops through an array and searches for the target, 
+//if it finds it, it returns the index
+//if the number is in between two numbers
+// it adds the number after the less number and return its index
+// if the number at the beginning is more than the target, 
+// it adds the number at the beginning and returns index
+//if the number at the end of the array is less than the target it returns the index if the last number +1
 var searchInsert = function (nums, target) {
     for (i = 0; i < nums.length; i++) {
         if (target === nums[i]) {
